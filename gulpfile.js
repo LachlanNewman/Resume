feca029f029ad507f9a7483c48fccec89f9ad77a
LachlanNewman <lachnewman007@gmail.com>
2018-11-14 14:58:04 +1100

    Stylized Resume

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
-const gulp = require('gulp'),
-      sass = require('gulp-sass');
+const gulp  = require('gulp'),
+      sass  = require('gulp-sass');
 
-const sassFiles = './sass/styles.scss';
-const cssDest   = './';
+const sassSrc     = './sass/styles.scss';
+const sassGlob    = './sass/**/*.scss';
+const cssDest     = './';
 
 gulp.task('sass', function () {
-    return gulp.src(sassFiles)
-        .pipe(sass()) // Converts Sass to CSS with gulp-sass
-        .pipe(gulp.dest(cssDest))
+      return gulp.src(sassSrc)
+            .pipe(sass()) // Converts Sass to CSS with gulp-sass
+            .pipe(gulp.dest(cssDest))
 });
 
-gulp.task('watch',function () {
-    gulp.watch(sassFiles, gulp.series('sass'));
+gulp.task('watch', function () {
+      gulp.watch(sassGlob, gulp.series('sass'));
 });
